b924c6807f2dba1e97b7a682e22ce8dd7a16e880
t.inoue <takayuki.inoue@i-studio.co.jp>
2022-11-08 05:33:34 +0900

    とりあえず使えるところまで作成

--- src/createIcons.ts
+++ src/createIcons.ts
@@ -1,5 +1,5 @@
 import Webfont from "webfont";
-import {GlyphData, InitialOptions} from "webfont/dist/src/types/index";
+import {GlyphData, InitialOptions, GlyphMetadata, Format} from "webfont/dist/src/types/index";
 import Deepmerge from "deepmerge";
 import {isPlainObject} from "is-plain-object";
 import fs from "fs";
@@ -8,72 +8,95 @@ import {colorCyan, colorMagenta, colorRed} from "./utils";
 import {Result} from "webfont/dist/src/types/Result";
 import script from "./script";
 
-const defaultOptions: InitialOptions = {
+export type customInitialOptions = InitialOptions & {
+  files: string | string[],
+  formats: Array<Format>,
+  fontName: string,
+  sort: boolean
+}
+
+const defaultOptions: customInitialOptions = {
   files: "./icons/**/*.svg",
   fontName: "",
-  formats: [],
+  formats: ["woff2", "woff"],
   sort: true,
 }
+class Graphicer {
+  private readonly outDir: string;
+  private options: customInitialOptions;
+  private readonly inputDir: string;
+  private readonly outputFormats: Array<Format>
+  private style: string;
+  constructor(inputOption: Partial<customInitialOptions>, outDir: string, css: boolean) {
+    this.outDir = outDir;
+    this.inputDir = typeof inputOption.files === "string" ? inputOption.files.replace("/**/*.svg", "") : inputOption.files![0].replace("/**/*.svg", "");
+    this.options = Deepmerge(defaultOptions, inputOption, {
+      isMergeableObject: isPlainObject
+    }) as customInitialOptions;
+    this.outputFormats =this.options.formats;
+    this.options.formats = this.options.formats!.concat(["woff2"]);
+    this.style = "";
+    this.createWebfontFile().then(() => {
+      if (css) {
+          this.createStyleSheet()
+      }
+    })
+  }
 
-
-const createWebfontFile = async(inputOption: Partial<InitialOptions>, outDir: string) => {
-  const options = Deepmerge(defaultOptions, inputOption, {
-    isMergeableObject: isPlainObject
-  }) as InitialOptions;
-  const prefixOutDir = path.resolve(outDir)
-  const fonts = await Webfont(options)
-  const metaDatum = [] as Array<{
-    path: string,
-    name: string,
-    unicode: Array<string>,
-    renamed: boolean,
-    width: number,
-    height: number,
-    color: string
-  }>;
-  if (fonts.glyphsData) {
-    for (let i = 0; i < fonts.glyphsData.length; i++) {
-      if (fonts.glyphsData[i].metadata) {
-        // @ts-ignore
-        metaDatum.push(fonts.glyphsData[i].metadata)
+  createWebfontFile = async() => {
+    const prefixOutDir = path.resolve(this.outDir)
+    const fonts = await Webfont(this.options)
+    const metaDatum = [] as Array<{
+      path?: string,
+      name: string,
+      unicode?: Array<string>,
+      renamed?: boolean,
+      width?: number,
+      height?: number,
+      color?: string
+    }>;
+    if (fonts.glyphsData) {
+      for (let i = 0; i < fonts.glyphsData.length; i++) {
+          const meta = fonts.glyphsData[i].metadata as GlyphMetadata
+          metaDatum.push(meta)
       }
     }
-  }
-  // @ts-ignore
-  createHTMLFile(metaDatum, options.fontName, options.files);
+    this.createHTMLFile(metaDatum);
 
-  for (const format of options.formats!) {
-    const fontData = fonts[format];
-    if (fontData) {
-      fs.writeFileSync(path.join(prefixOutDir, `${options.fontName}.${format}`), fontData);
-    } else {
-      console.log(colorRed("Cannot create Webfont File.\nPlease Check Setting or SVG File."))
+    for (const format of this.outputFormats) {
+      const fontData = fonts[format];
+      if (fontData) {
+        const outputFile = path.join(prefixOutDir, `${this.options.fontName}.${format}`)
+        this.diggerDirectory(outputFile)
+        fs.writeFileSync(outputFile, fontData);
+      } else {
+        console.log(colorRed("Cannot create Webfont File.\nPlease Check Setting or SVG File."))
+      }
     }
+    const outputFile = path.join(this.inputDir,"/example/", `${this.options.fontName}.woff2`)
+    this.diggerDirectory(outputFile)
+    fs.writeFileSync(outputFile, fonts.woff2!);
   }
-}
 
-const createCache = (inputDir: string) => {
-  fs.writeFileSync(``, "", "utf-8");
-}
+  createCache = (inputDir: string) => {
+    fs.writeFileSync(``, "", "utf-8");
+  }
 
-const createHTMLFile = async(metadata: Array<{
-  path?: string,
-  name?: string,
-  unicode?: Array<string>,
-  renamed?: boolean,
-  width?: number,
-  height?: number,
-  color?: string
-}>, fontName: string, inputDir: string) => {
-  const fontTypes = [];
-  for (const data of metadata) {
-    fontTypes.push(data.name)
-  }
-  const windowObject = {
-    fontName,
-    fontTypes
-  }
-  const html = `
+  createHTMLFile = async(metadata: Array<{
+    path?: string,
+    name?: string,
+    unicode?: Array<string>,
+    renamed?: boolean,
+    width?: number,
+    height?: number,
+    color?: string
+  }>) => {
+    const fontTypes = [];
+    for (const data of metadata) {
+      fontTypes.push(data.name)
+      this.style = this.style + `.${data.name}::after {content:"${data.name}"}\n`;
+    }
+    const html = `
   <!DOCTYPE html>
   <html>
     <head>
@@ -82,11 +105,14 @@ const createHTMLFile = async(metadata: Array<{
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Mr.Graphicer</title>
-      <script>window.fontdata=${JSON.stringify(windowObject)}</script>
+      <script>window.fontdata=${JSON.stringify({
+      fontName: this.options.fontName,
+      fontTypes
+    })}</script>
       <style>
       @font-face {
-        font-family: "${fontName}";
-        src: url("./${fontName}.woff2");
+        font-family: "${this.options.fontName}";
+        src: url("./${this.options.fontName}.woff2");
       }
 </style>
     </head>
@@ -96,31 +122,33 @@ const createHTMLFile = async(metadata: Array<{
     </body>
   </html>
   `
-  const outputHTMLPath = path.join(inputDir.replace("/**/*.svg", ""),"example/index.html")
-  const createDir = `${outputHTMLPath.replace("index.html", "")}`
-  if (!fs.existsSync(`${createDir}`)) {
-    fs.mkdirSync(`${outputHTMLPath.replace("index.html", "")}`)
+    const outputHTMLPath = path.join(this.inputDir,"example/index.html")
+    const createDir = `${outputHTMLPath.replace("index.html", "")}`
+    this.diggerDirectory(createDir)
+    fs.writeFileSync(outputHTMLPath,html, "utf-8")
+    fs.writeFileSync(outputHTMLPath.replace("index.html", "bundle.js"),script, "utf-8")
   }
-  fs.writeFileSync(outputHTMLPath,html, "utf-8")
-  fs.writeFileSync(outputHTMLPath.replace("index.html", "bundle.js"),script, "utf-8")
-}
 
-const diggerDirectory = async(pathName: string) => {
-  const ex = path.extname(pathName)
-  console.log(pathName)
-  const fileDir = pathName.split('/').filter((val) => {
-    if (val !== '' && !val.includes(ex)) return val
-  })
-  console.log(fileDir)
-  let outPutDir = '/'
-  for (const innerDir of fileDir) {
-    outPutDir = path.join(outPutDir, innerDir)
-    if (!fs.existsSync(outPutDir)) {
-      await fs.mkdirSync(outPutDir)
+  createStyleSheet = () => {
+    fs.writeFileSync(path.join(this.outDir, "icon-font.css"), this.style, "utf-8")
+  }
+
+
+  diggerDirectory = async(pathName: string) => {
+    const ex = path.extname(pathName)
+    const fileDir = pathName.split('/').filter((val) => {
+      if (val !== '' && !val.includes(ex)) return val
+    })
+    let outPutDir = '/'
+    for (const innerDir of fileDir) {
+      outPutDir = path.join(outPutDir, innerDir)
+      if (!fs.existsSync(outPutDir)) {
+        await fs.mkdirSync(outPutDir)
+      }
     }
   }
 }
 
 
 
-export default createWebfontFile;
+export default Graphicer;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 import { program } from "commander";
-import createIcons from "./createIcons";
+import Graphicer, {customInitialOptions} from "./createIcons";
 import path from "path";
-import {InitialOptions} from "webfont/dist/src/types/index";
+
+interface optsInterface {
+  input: string,
+  output: string,
+  name: string,
+  format: string,
+  css: boolean
+}
 
 program
   .option("-i, --input <value>", "Input Directory.", "./icons")
   .option("-o,--output <value>", "Output Directory.", "./")
   .option("-n, --name <value>", "Icon font name.", "icon-font")
   .option("-f, --format <value>", "Icon file format. ex) ttf,woff,woff2", "ttf,eot,woff,woff2")
+  .option("-c, --css", "output css file.", false)
 
 program.parse();
 
-const opts = program.opts() as Record<string, string>;
+const opts = program.opts() as optsInterface;
 
-const targetDirFiles = opts.input + "/**/*.svg"
+const targetDirFiles = path.resolve(path.join(opts.input, "/**/*.svg"))
+console.log(targetDirFiles)
 
-const options: Partial<InitialOptions> = {
-  files: targetDirFiles,
+const options: Partial<customInitialOptions> = {
+  files: targetDirFiles as string,
   fontName: opts.name,
   formats: opts.format.split(",") as Array<"woff" | "woff2" | "svg" | "eot">,
 }
 
-createIcons(options, opts.output).then()
+new Graphicer(options, opts.output, opts.css)
